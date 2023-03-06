058dc2a455e54d0571675aed63119deef14d9bec
Skip Baney <twelvelabs@gmail.com>
2023-03-05 19:33:36 -0600

    make bashrc cross-plaform

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ set -o errexit -o errtrace -o nounset -o pipefail
 
 # Install dotfiles
 echo "[install] Installing dotfiles."
-RCRC="./rcm/rcrc" rcup -v
+RCRC="./rcm/rcrc" rcup -v -f
 
 echo "[install] Done 🎉"
