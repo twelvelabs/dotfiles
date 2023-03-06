63c18039c239a1d61d6da1aadf82442a633c6852
Skip Baney <twelvelabs@gmail.com>
2023-03-06 07:56:40 -0600

    update to authenticated git remote post install

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit -o errtrace -o nounset -o pipefail
 
-# Bootstrap
 ./bootstrap.sh
 
-# Install dotfiles
 echo "[install] Installing dotfiles."
 RCRC="./rcm/rcrc" rcup -v -f
 
+echo "[install] Updating dotfiles Git remote."
+git remote set-url origin git@github.com:twelvelabs/dotfiles.git
+
 echo "[install] Done 🎉"
+exit 0
