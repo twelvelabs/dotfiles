159bccc16b4cbb2c19d06fb8259e7de24466fbbc
Skip Baney <twelvelabs@gmail.com>
2023-04-16 15:51:41 -0500

    only install packages when needed on Linux

--- bootstrap.d/tag-linux/001-packages.sh
+++ bootstrap.d/tag-linux/001-packages.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 set -o errexit -o errtrace -o nounset -o pipefail
 
-sudo apt-get update
-sudo apt-get install -y rcm
+function log() {
+    local msg="$1"
+    echo "[bootstrap][linux][packages] ${msg}"
+}
+
+updated=""
+dependencies=(
+    "rcm"
+)
+for dependency in "${dependencies[@]}"; do
+    if command -v "${dependency}" >/dev/null 2>&1; then
+        log "Found dependency: ${dependency}"
+    else
+        if [[ "${updated}" != "true" ]]; then
+            log "Updating package index"
+            sudo apt-get update
+            updated="true"
+        fi
+        log "Installing dependency: ${dependency}"
+        sudo apt-get install -y "${dependency}"
+    fi
+done
