1c3f7c3fa4dc0b09485d14d34369bb3204642b13
Skip Baney <twelvelabs@gmail.com>
2023-04-19 15:09:27 -0500

    read hostname after setting

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit -o errtrace -o nounset -o pipefail
 
-platform=$(uname -s | tr '[:upper:]' '[:lower:]')
-hostname=$(hostname)
-
 echo "[bootstrap] Running default scripts."
 for path in bootstrap.d/default/*.sh; do
     if [[ "$path" == "bootstrap.d/default/*.sh" ]]; then
@@ -12,6 +9,7 @@ for path in bootstrap.d/default/*.sh; do
     echo "$path"
 done
 
+platform=$(uname -s | tr '[:upper:]' '[:lower:]')
 echo "[bootstrap][${platform}] Running platform scripts."
 for path in "bootstrap.d/tag-${platform}/"*.sh; do
     if [[ "$path" == "bootstrap.d/tag-${platform}/*.sh" ]]; then
@@ -20,6 +18,9 @@ for path in "bootstrap.d/tag-${platform}/"*.sh; do
     "$path"
 done
 
+# Important that this be calculated here, because the platform scripts
+# may have updated the hostname.
+hostname=$(hostname)
 echo "[bootstrap][${hostname}] Running host scripts."
 for path in "bootstrap.d/host-${hostname}/"*.sh; do
     if [[ "$path" == "bootstrap.d/host-${hostname}/*.sh" ]]; then
