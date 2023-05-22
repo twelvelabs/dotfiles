90bf21754e4cb4f4db0b2bd3cac407e88a93ab9a
Skip Baney <twelvelabs@gmail.com>
2023-05-22 10:53:26 -0500

    typo fix

--- bootstrap.d/tag-linux/001-packages.sh
+++ bootstrap.d/tag-linux/001-packages.sh
@@ -6,17 +6,17 @@ function log() {
     echo "[bootstrap][linux][packages] ${msg}"
 }
 
+sudo=""
+if [[ "$(whoami)" != "root" ]]; then
+    sudo="sudo"
+fi
+
 function install-alpine() {
     local dependency="$1"
 
-    if "$(apk list --installed --quiet "${dependency}")" != ""; then
+    if [[ "$(apk list --installed --quiet "${dependency}")" != "" ]]; then
         log "Found dependency: ${dependency}"
     else
-        if [[ "${updated}" != "true" ]]; then
-            log "Updating package index"
-            $sudo apk update
-            updated="true"
-        fi
         log "Installing dependency: ${dependency}"
         $sudo apk add --no-cache "${dependency}"
     fi
@@ -28,22 +28,11 @@ function install-deb() {
     if dpkg -l "${dependency}" &>/dev/null; then
         log "Found dependency: ${dependency}"
     else
-        if [[ "${updated}" != "true" ]]; then
-            log "Updating package index"
-            $sudo apt-get update
-            updated="true"
-        fi
         log "Installing dependency: ${dependency}"
         $sudo apt-get install -y "${dependency}"
     fi
 }
 
-sudo=""
-if [[ "$(whoami)" != "root" ]]; then
-    sudo="sudo"
-fi
-
-updated=""
 dependencies=(
     "gum"
     "jq"
